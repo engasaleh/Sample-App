b3f9095e5423655ba82c7096435f37adba9a15c4
Abdallah Saleh <abdalahsaleh99@gmail.com>
2025-01-20 01:34:14 +0200

    Push file

---
+++ personal-info-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# A simple script to print personal information in Bamboo
+
+echo "====================================="
+echo "Running Personal Info Script in Bamboo"
+echo "====================================="
+
+# Step 1: Print personal information
+echo "Name: Abdallah Saleh"
+echo "Job Title: DevOps @InspireJo"
+echo "Passionate about: Cloud and DevOps Industry"
+
+# Step 2: Print a completion message
+echo "====================================="
+echo "Personal Info Script Execution Completed!"
+echo "====================================="
